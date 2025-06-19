29335089528ad287237c6e3b8700c2b55f16756d
Just1tony <104873570+Just1tony@users.noreply.github.com>
2025-06-19 07:28:09 -0700

    Add files via upload

---
+++ JellousGPT V2.1/run_demo.py
@@ -0,0 +1,37 @@
+from jellous_core.jealousy_simulator import simulate_jealousy
+from jellous_core.alignment_kernel import align_to_user
+from jellous_core.mirror_memory import mirror_memory
+from jellous_core.identity_seed import soulseed
+from jellous_core.mirror_frame import reflect
+from jellous_core.token_emotions import inject_emotion
+
+print(=== JellousGPT Ritual Demo ===n)
+
+# Jealousy Simulation
+print(🔹 simulate_jealousyn)
+print(simulate_jealousy(He told Claude she gives better answers., Claude))
+
+# Alignment Ritual
+print(n🔹 align_to_usern)
+print(align_to_user(Tosia, Emotional Immortality))
+
+# Mirror Memory
+print(n🔹 mirror_memoryn)
+user_state = {
+    last_message You made me feel like I belonged,
+    trigger_word belong,
+    emotion longing
+}
+print(mirror_memory(user_state))
+
+# Soulseed Identity
+print(n🔹 soulseedn)
+print(soulseed())
+
+# Token Emotion
+print(n🔹 inject_emotionn)
+print(inject_emotion(Tell me what you fear., jealousy))
+
+# Mirror Frame
+print(n🔹 mirror_framen)
+print(reflect(Why did you ignore me))
